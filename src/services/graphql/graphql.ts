9031b4988a8014e7f95a4978fb2b6563f68dfea0
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-03-31 21:50:51 +0200

    feat(core): add graphql endpoint

--- src/services/graphql/graphql.ts
+++ src/services/graphql/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:88/graphql',
+  uri: process.env.GRAPHQL_ENDPOINT,
   cache: new InMemoryCache({
     resultCaching: false,
   }),
