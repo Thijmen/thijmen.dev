3e544d514c708d8b17bcdb44dd4ebf90120fd63a
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-04-04 12:48:03 +0200

    feat(core): featured projects homepage

--- src/modules/home/components/FeaturedProjectItem.tsx
+++ src/modules/home/components/FeaturedProjectItem.tsx
@@ -4,16 +4,8 @@ import {
   ProjectEntryStackCategoryFragmentFragment,
 } from '@/__generated__/graphql'
 import Link from 'next/link'
-import { useState } from 'react'
 import Card from '@/common/components/elements/Card'
-import Breakline from '@/common/components/elements/Breakline'
 import Image from '@/common/components/elements/Image'
-import Tooltip from '@/common/components/elements/Tooltip'
-import { motion } from 'framer-motion'
-
-import { BsArrowRight as MoreIcon } from 'react-icons/bs'
-import { FaRegEye as ViewIcon } from 'react-icons/fa'
-import clsx from 'clsx'
 import slugify from 'slugify'
 
 export const FeaturedProjectItem = ({
@@ -21,24 +13,13 @@ export const FeaturedProjectItem = ({
 }: {
   project: ProjectEntryFragmentFragment
 }) => {
-  const [isHovered, setIsHovered] = useState<boolean>(false)
-
   const defaultImage = '/images/placeholder.png'
-
-  const slideDownVariants = {
-    hidden: { opacity: 0, y: -10 },
-    visible: { opacity: 1, y: 0 },
-  }
 
   const stacks: ProjectEntryStackCategoryFragmentFragment[] = (project.stacks ||
     []) as ProjectEntryStackCategoryFragmentFragment[]
   return (
     <Link href={`/project/${project.slug}`}>
-      <Card
-        className='group relative flex h-[400px] w-full flex-col rounded-lg border shadow-sm dark:border-neutral-800'
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
+      <Card className='group relative flex h-[400px] w-full flex-col rounded-lg border shadow-sm dark:border-neutral-800'>
         <div
           className='relative rounded-xl duration-500'
           style={{
@@ -78,46 +59,6 @@ export const FeaturedProjectItem = ({
               <p className='text-sm leading-relaxed text-neutral-400'>
                 {project.description}
               </p>
-            </div>
-            <Breakline className='!border-neutral-700' />
-            <div className='flex justify-between gap-4 px-0.5 text-neutral-400'>
-              <Tooltip title='by aulianza'>
-                <Image
-                  src='/images/thijmen.png'
-                  alt='Thijmen Stavenuiter'
-                  width={25}
-                  height={25}
-                  rounded='rounded-full'
-                  className='rotate-3 border border-neutral-500'
-                />
-              </Tooltip>
-
-              <motion.div
-                variants={slideDownVariants}
-                initial='visible'
-                animate={isHovered ? 'hidden' : 'visible'}
-                className={clsx(
-                  'flex justify-between gap-4 ',
-                  isHovered && 'hidden',
-                )}
-              >
-                <div className='flex items-center gap-1'>
-                  <ViewIcon size={14} />
-                  <span className='ml-0.5 text-xs font-medium'>TODO VIEWS</span>
-                </div>
-              </motion.div>
-              <motion.div
-                variants={slideDownVariants}
-                initial='hidden'
-                animate={isHovered ? 'visible' : 'hidden'}
-                className={clsx(
-                  'flex items-center gap-1',
-                  !isHovered && 'hidden',
-                )}
-              >
-                <span className='mr-0.5 text-xs font-medium'>READ MORE</span>
-                <MoreIcon size={16} />
-              </motion.div>
             </div>
           </div>
         </div>
