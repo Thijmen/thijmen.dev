01d9a4753b7e46ab00172befeeb34473b34bcd59
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-04-12 17:26:05 +0200

    feat(core): reworked layout

--- src/common/components/layouts/index.tsx
+++ src/common/components/layouts/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import clsx from 'clsx'
-import { usePathname } from 'next/navigation'
 import { useTheme } from 'next-themes'
 import { ReactNode } from 'react'
 import { useWindowSize } from 'usehooks-ts'
@@ -16,44 +15,45 @@ interface LayoutProps {
   isFullPageHeader?: boolean
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, isFullPageHeader }: LayoutProps) => {
   const { resolvedTheme } = useTheme()
   const hasMounted = useHasMounted()
-  const pathname = usePathname()
   const { width } = useWindowSize()
   const isMobile = width < 480
 
   const isDarkTheme =
     hasMounted && (resolvedTheme === 'dark' || resolvedTheme === 'system')
 
-  const pageName = pathname.split('/')[1]
-
-  const isFullPageHeader =
-    pageName === 'playground' ||
-    // pageName === 'blog' ||
-    pathname.startsWith('/blog/') ||
-    pathname.startsWith('/learn/')
+  if (isFullPageHeader) {
+    return (
+      <>
+        <FullHeaderNav />
+        <div
+          className={clsx(
+            'mx-auto max-w-6xl lg:px-8',
+            isDarkTheme ? 'dark:text-darkText' : '',
+          )}
+        >
+          {children}
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
-      {/* <TopBar /> */}
-      {isFullPageHeader && <FullHeaderNav />}
       <div
         className={clsx(
           'mx-auto max-w-6xl lg:px-8',
           isDarkTheme ? 'dark:text-darkText' : '',
         )}
       >
-        {isFullPageHeader ? (
-          <>{children}</>
-        ) : (
-          <div className='flex flex-col lg:flex-row lg:gap-5 lg:py-4 xl:pb-8'>
-            <HeaderSidebar />
-            <main className='max-w-[854px] transition-all duration-300 lg:w-4/5'>
-              {children}
-            </main>
-          </div>
-        )}
+        <div className='flex flex-col lg:flex-row lg:gap-5 lg:py-4 xl:pb-8'>
+          <HeaderSidebar />
+          <main className='max-w-[854px] transition-all duration-300 lg:w-4/5'>
+            {children}
+          </main>
+        </div>
       </div>
       {isMobile ? <NowPlayingCard /> : <NowPlayingBar />}
     </>
