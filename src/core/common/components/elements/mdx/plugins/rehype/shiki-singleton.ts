894892d99770b352ee4a15dd317cd7c2e0de7358
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-09-18 13:43:07 +0200

    feat(chore): fix shiki singleton

--- src/core/common/components/elements/mdx/plugins/rehype/shiki-singleton.ts
+++ src/core/common/components/elements/mdx/plugins/rehype/shiki-singleton.ts
@@ -1,25 +1,25 @@
-import { createHighlighter, type Highlighter } from "shiki";
+import { type Highlighter, createHighlighter } from "shiki";
 
 export class ShikiSingleton {
-  private static instance: ShikiSingleton;
-  private highlighter: Highlighter | null = null;
+	private static instance: ShikiSingleton;
+	private highlighter: Highlighter | null = null;
 
-  private constructor() {}
+	private constructor() {}
 
-  public static getInstance(): ShikiSingleton {
-    if (!ShikiSingleton.instance) {
-      ShikiSingleton.instance = new ShikiSingleton();
-    }
-    return ShikiSingleton.instance;
-  }
+	public static getInstance(): ShikiSingleton {
+		if (!ShikiSingleton.instance) {
+			ShikiSingleton.instance = new ShikiSingleton();
+		}
+		return ShikiSingleton.instance;
+	}
 
-  public async getHighlighter(param: { themes: string[]; langs: string[] }) {
-    if (!this.highlighter) {
-      this.highlighter = await createHighlighter({
-        themes: param.themes,
-        langs: param.langs,
-      });
-    }
-    return this.highlighter;
-  }
+	public async getHighlighter(param: { themes: string[]; langs: string[] }) {
+		if (!this.highlighter) {
+			this.highlighter = await createHighlighter({
+				themes: param.themes,
+				langs: param.langs,
+			});
+		}
+		return this.highlighter;
+	}
 }
