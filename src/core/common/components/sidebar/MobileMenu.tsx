2a7f8e0f9678ac933a615ac7fc3304877bee274d
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-09-21 09:56:52 +0200

    wip

--- src/core/common/components/sidebar/MobileMenu.tsx
+++ src/core/common/components/sidebar/MobileMenu.tsx
@@ -1,26 +1,26 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion'
 
-import Breakline from "../elements/Breakline";
-import Navigation from "./Navigation";
-import type { Nav } from "@/payload/payload-types";
+import type { Nav } from '@/payload/payload-types'
+import Breakline from '../elements/Breakline'
+import Navigation from './Navigation'
 
 interface Props {
-  navGlobal: Nav;
+	navGlobal: Nav
 }
 const MobileMenu = (props: Props) => {
-  return (
-    <motion.div
-      className="my-3 flex h-screen flex-col"
-      initial={{ y: -100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div>
-        <Breakline className="mt-2" />
-        <Navigation navGlobal={props.navGlobal} />
-      </div>
-    </motion.div>
-  );
-};
+	return (
+		<motion.div
+			className='my-3 flex h-screen flex-col'
+			initial={{ y: -100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.3 }}
+		>
+			<div>
+				<Breakline className='mt-2' />
+				<Navigation navGlobal={props.navGlobal} />
+			</div>
+		</motion.div>
+	)
+}
 
-export default MobileMenu;
+export default MobileMenu
