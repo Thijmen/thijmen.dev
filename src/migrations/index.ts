f48dd57f5b3744a3212209758533fea44b861ac2
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-29 12:11:15 +0100

    Add migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241016_175315 from './20241016_175315';
+import * as migration_20241029_111051 from './20241029_111051';
 
 export const migrations = [
   {
     up: migration_20241016_175315.up,
     down: migration_20241016_175315.down,
-    name: '20241016_175315'
+    name: '20241016_175315',
+  },
+  {
+    up: migration_20241029_111051.up,
+    down: migration_20241029_111051.down,
+    name: '20241029_111051'
   },
 ];
