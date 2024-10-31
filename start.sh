1e9322adbd7e0582a26268266654bddf52d5abaa
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-10-31 20:19:31 +0100

    Add start.sh env args

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-docker run -p 3000:3000 website
+ENV_ARGS=""
+while IFS='=' read -r key value; do
+    # Skip comments and empty lines
+    [[ $key =~ ^#.*$ ]] && continue
+    [[ -z $key ]] && continue
+    
+    # Remove quotes from value if present
+    value=$(echo "$value" | sed -e 's/^"//' -e 's/"$//' -e "s/^'//" -e "s/'$//")
+    
+    # Add each env var as a build arg
+    ENV_ARGS="$ENV_ARGS -e $key=$value"
+done < .env
+
+docker run $ENV_ARGS -p 3001:3000 website
