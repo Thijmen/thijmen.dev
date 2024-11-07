d56c7d964a4604a332ac01951671f51779247982
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-11-07 21:18:16 +0100

    Change to correct port

--- src/payload/validation/mdx/index.ts
+++ src/payload/validation/mdx/index.ts
@@ -1,6 +1,6 @@
 export const validateMDX = async (source: string) => {
 	// create a fetch call to /api/test with the source as body
-	const response = await fetch('http://localhost:3000/api/test', {
+	const response = await fetch('http://localhost:3001/api/test', {
 		method: 'POST',
 		headers: {
 			'X-Secure-Token': process.env.SECURE_VALIDATION_TOKEN,
