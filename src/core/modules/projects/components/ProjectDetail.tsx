76ca292c4bff7f35e879d1b0a3951ac13cc3216e
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2025-01-09 08:54:59 +0100

    Refactor ProjectDetail component to replace SharedContent with RichText for dynamic content rendering
    - Updated ProjectDetail.tsx to import and utilize RichText from @payloadcms/richtext-lexical/react.
    - Replaced the SharedContent component with RichText to enhance the handling of dynamic content within the project details.
    
    This change improves the flexibility and rendering capabilities of dynamic content in the ProjectDetail component.

--- src/core/modules/projects/components/ProjectDetail.tsx
+++ src/core/modules/projects/components/ProjectDetail.tsx
@@ -1,9 +1,9 @@
 import Tooltip from '@/core/common/components/elements/Tooltip'
 import { getStackIcon } from '@/core/common/constant/stacks'
 
-import { SharedContent } from '@/core/common/components/shared-content'
 import type { Project, R2Media, Stack } from '../../../../payload/payload-types'
 import ProjectLink from './ProjectLink'
+import { RichText } from '@payloadcms/richtext-lexical/react'
 
 const ProjectDetail = ({ project }: { project: Project }) => {
 	const image: string =
@@ -40,7 +40,7 @@ const ProjectDetail = ({ project }: { project: Project }) => {
 			</div>
 			{project.dynamiccontent && (
 				<div className='mt-5 space-y-6 leading-[1.8] dark:text-neutral-300'>
-					<SharedContent content={project.dynamiccontent} />
+					<RichText data={project.dynamiccontent} />
 				</div>
 			)}
 		</div>
